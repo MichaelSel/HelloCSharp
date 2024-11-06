e4e61149515346f973c55636ddab2fe0e4150103
MichaelSel <seltenmusic@gmail.com>
2024-11-06 18:23:48 -0500

    Routing

--- HelloWorld/Controllers/ProductsController.cs
+++ HelloWorld/Controllers/ProductsController.cs
@@ -4,11 +4,11 @@ using HelloWorld.Routes;
 
 namespace HelloWorld.Controllers;
 
-[Route(ApiRoutes.Products.GetAll)]
-[ApiController]
+[Route(ApiRoutes.Products.GetAll)] // Attribute indication -> This tells the framework that this controller should be called when a request is made to the specified route
+[ApiController] // Attribute indication -> This tells the framework that this controller should be treated as a controller for API requests
 public class ProductsController(IProductService productService) : ControllerBase
 {
-    [HttpGet]
+    [HttpGet] // Attribute indication -> This tells the framework that this method should be called when an HTTP GET request is made to the specified route
     public IActionResult GetAllProducts()
     {
         var products = productService.GetAllProducts();
@@ -21,9 +21,38 @@ public class ProductsController(IProductService productService) : ControllerBase
 public class ProductController(IProductService productService) : ControllerBase
 {
     [HttpGet]
-    public IActionResult GetProductById(int id)
+    public IActionResult GetProductById(int id) // The id parameter is automatically bound to the value in the route
     {
         var products = productService.GetProductById(id);
         return Ok(products);
+    }
+}
+
+
+
+
+
+// More below
+
+[Route("another-route")] // This can also be a literal string
+[ApiController]
+public class AnotherController : ControllerBase
+{
+    [HttpGet("first")] // This is a route template that appends to the route specified in the class attribute (so "another-route/first")
+    public IActionResult Get()
+    {
+        return Ok("Get from another route!");
+    }
+    
+    [HttpPost("first")] // This is a route template that appends to the route specified in the class attribute (so "another-route/first")
+    public IActionResult Post()
+    {
+        return Ok("Post from another route!");
+    }
+    
+    [HttpPut("second")] // This is a route template that appends to the route specified in the class attribute (so "another-route/second")
+    public IActionResult Put()
+    {
+        return Ok("Put from yet another route!");
     }
 }
